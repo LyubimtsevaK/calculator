21bdaa42a4c2c8499a27ac016da9ab46691af482
LyubimtsevaK <lyubimtseva.k9@gmail.com>
2023-10-22 19:33:16 +0400

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import InputNumber from './Components/InputNumber.js';
+import OutputNumber from './Components/OutputNumber.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState("");
 
+  const buttonNumber = (value) => {
+    setInputValue((prevInputValue) => prevInputValue + value);
+  }
+
+  const clearInput = () => {
+    setInputValue('');
+  }
+
+
+  const math = require('mathjs');
+
+  const calculateResult = () => {
+    try {
+      const evaluatedResult = math.evaluate(inputValue);
+      setResult(evaluatedResult.toString());
+    } catch (error) {
+      setResult("Ошибка при вычислении");
+    }
+  };
+
+  return (
+    <div>
+      <div className='container'>
+        <InputNumber type="text" value={inputValue} buttonNumber={buttonNumber}/>
+        <OutputNumber type="text" value={result} readOnly/>
+      </div>
+      <div className='button'>
+        <button className='butt' onClick={clearInput}>С</button>
+        <button className='butt1' onClick={clearInput}>🠔</button>
+        <button className='butt' onClick={() => buttonNumber('%')}>%</button>
+        <button className='butt' onClick={() => buttonNumber('/')}>/</button>
+      </div>
+      <div>
+        <button className='butt' onClick={() => buttonNumber('7')}>7</button>
+        <button className='butt' onClick={() => buttonNumber('8')}>8</button>
+        <button className='butt' onClick={() => buttonNumber('9')}>9</button>
+        <button className='butt' onClick={() => buttonNumber('*')}>*</button>
+      </div>
+      <div>
+        <button className='butt' onClick={() => buttonNumber('4')}>4</button>
+        <button className='butt' onClick={() => buttonNumber('5')}>5</button>
+        <button className='butt' onClick={() => buttonNumber('6')}>6</button>
+        <button className='butt' onClick={() => buttonNumber('-')}>-</button>
+      </div>
+      <div>
+        <button className='butt' onClick={() => buttonNumber('1')}>1</button>
+        <button className='butt' onClick={() => buttonNumber('2')}>2</button>
+        <button className='butt' onClick={() => buttonNumber('3')}>3</button>
+        <button className='butt' onClick={() => buttonNumber('+')}>+</button>
+      </div>
+      <div>
+        <button className='butt2' onClick={() => buttonNumber('0')}>0</button>
+        <button className='butt' onClick={() => buttonNumber('.')}>.</button>
+        <button className='butt' onClick={calculateResult}>=</button>
+      </div>
+    </div>
+  )
+}
 export default App;
---
+++ src/Components/OutputNumber.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const InputNumber = ({ value, buttonNumber }) => {
+    const handleChange = (event) => {
+    const newValue = event.target.value;
+    buttonNumber(newValue);
+    console.log(typeof(buttonNumber(newValue)));
+    }
+    return (
+        <div>
+            <input 
+                className='output'
+                type="text"
+                value={value}
+                onChange={handleChange}
+            />
+        </div>
+    );
+};
+
+export default InputNumber;
